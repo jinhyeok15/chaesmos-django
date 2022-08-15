da846e2553e0e7f2cbc96aec06f523e06e31b1fb
jinhyeok15 <ekfrhd97@gmail.com>
2022-08-16 02:03:45 +0900

    staticfiles 관련 setting수정

--- webapp/chaesmos/chaesmos/settings.py
+++ webapp/chaesmos/chaesmos/settings.py
@@ -149,9 +149,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' if not env('PRODUCTION') else '/static_files/'
+
+if env('PRODUCTION'):
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = '/srv/static-files'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
