0283fca4b7801beee7707fbdf441326d3e2d7090
jinhyeok15 <ekfrhd97@gmail.com>
2022-08-20 00:27:23 +0900

    chore: sh 파일 수정

--- mvconf.sh
+++ mvconf.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 mv config/config_list/.env config/environment/
-mv config/config_list/.env webapp/chaesmos/
+# mv config/config_list/.env webapp/chaesmos/
+
+rm webapp/chaesmos/settings.py
+mv config/config_list/prod_settings.py webapp/chaesmos/settings.py
+
 mv config/config_list/config.js webapp/chaesmos/static/js/
