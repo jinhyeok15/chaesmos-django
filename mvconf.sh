ea2c6f48041dc05551b405eefad15b2385c008b3
jinhyeok15 <ekfrhd97@gmail.com>
2022-08-20 00:55:11 +0900

    prod setting 환경 수정

--- mvconf.sh
+++ mvconf.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 mv config/config_list/.env config/environment/
-# mv config/config_list/.env webapp/chaesmos/
-
-rm webapp/chaesmos/settings.py
-mv config/config_list/prod_settings.py webapp/chaesmos/settings.py
+mv config/config_list/.env webapp/chaesmos/
 
 mv config/config_list/config.js webapp/chaesmos/static/js/
