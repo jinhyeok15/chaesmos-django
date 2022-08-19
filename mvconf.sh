8fdf0ee675b50267ec967752822360a382379a29
Jinhyeok, Lee <82345753+jinhyeok15@users.noreply.github.com>
2022-08-19 15:37:53 +0900

    Update mvconf.sh

--- mvconf.sh
+++ mvconf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mv config/config_list/.env config/environment/
-mv config/config_list/.env webapp/chaesmos/
+# mv config/config_list/.env webapp/chaesmos/
 mv config/config_list/config.js webapp/chaesmos/static/js/
